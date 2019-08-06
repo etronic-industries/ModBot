cc320d91eb51d94c4be7883b900becce13b8a550
TotallyNotNero <Neromisis2015@gmail.com>
2019-08-05 22:00:23 -0500

    my friend loves himself so much...

--- Commands/ping.js
+++ Commands/ping.js
@@ -13,7 +13,7 @@ const config = require("../Configuration/config.json");
 module.exports = async(bot, msg, args) => {
     const embed = new Discord.RichEmbed()
         .setAuthor(bot.user.username, bot.user.avatarURL)
-        .setDescription(`:ping_pong: **${bot.user.username}** v${config.version} by Ken#4234, serving ${bot.users.size} users and ${bot.guilds.size} servers`)
+        .setDescription(`:ping_pong: **${bot.user.username}** v${config.version} by TotallyNotNero#0420, serving ${bot.users.size} users and ${bot.guilds.size} servers`)
         .setFooter(`${bot.user.username} v${config.version}`)
         .setColor(0x00ff05)
 
